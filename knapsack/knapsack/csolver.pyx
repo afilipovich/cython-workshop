bc3f9df4771cf9900d88d96219e3b4768da7bfdf
Aleh Filipovich <aleh@appnexus.com>
2016-02-08 09:18:41 +0100

    Optimized cython code

--- knapsack/knapsack/csolver.pyx
+++ knapsack/knapsack/csolver.pyx
@@ -1,5 +1,51 @@
 import itertools
 
+from pprint import pprint
+from cpython.mem cimport PyMem_Malloc, PyMem_Realloc, PyMem_Free
+
+cdef struct sack_item:
+    double cost
+    double weight
+
+cdef class CSack:
+    cdef sack_item* csack
+    cdef int numitems
+    cdef double max_capacity
+
+    def __cinit__(self, sack):
+        self.numitems = len(sack['items'])
+        self.max_capacity = sack['capacity']
+        self.csack = <sack_item *>PyMem_Malloc(self.numitems * sizeof(sack_item))
+        if not self.csack:
+            raise MemoryError()
+        cdef i = 0
+        for i,item in enumerate(sack['items']):
+            self.csack[i].cost = item['cost']
+            self.csack[i].weight = item['weight']
+
+    def get_max_capacity(self):
+        return self.max_capacity
+
+    def get_numitems(self):
+        return self.numitems
+
+    def compute_weight_and_cost(self, combo_num):
+        cdef double weight = 0.0
+        cdef double cost = 0.0
+        cdef int i = 0
+        cdef int cn = combo_num
+        csack = self.csack
+
+        for i in range(self.numitems):
+            if (cn & 1 == 1): # last bit in combo number
+                cost += csack[i].cost
+                weight += csack[i].weight
+            cn >>= 1 # shift combo number bit-by-bit
+        return weight, cost
+
+    def __dealloc__(self):
+        PyMem_Free(self.csack)
+
 
 class CBruteSolver:
 
@@ -8,37 +54,31 @@ class CBruteSolver:
     '''
 
     def __init__(self, sack):
-        '''
-        Takes a sack dict as returned by DataLoader
-        '''
-        self.sack = sack
+        self.csack = CSack(sack)
 
-    def int2list(self, n):
+    def int2list(self, n): # calculate this to form the output, less efficient but runs only once
         l = []
-        for i in range(self.numitems):
-            l.append(n & 1 == 1)
-            n >>= 1
+        cdef int i = 0
+        cdef int cn = n
+        for i in range(self.csack.get_numitems()):
+            l.append((cn & 1 == 1))
+            cn >>= 1
         return l
-
-    def weight_and_cost(self, combo):
-        weight = cost = 0
-        for i, item in enumerate(self.sack['items']):
-            if combo[i]:
-                weight += item['weight']
-                cost += item['cost']
-        return weight, cost
 
     def solve(self):
         '''
         Main rutine, solves the problem,
         returns the combination with maximal cost and the cost
         '''
-        self.numitems = len(self.sack['items'])
-        self.maxcost = -1
-        for num in range(2 ** self.numitems):
-            combo = self.int2list(num)
-            weight, cost = self.weight_and_cost(combo)
-            if weight <= self.sack['capacity'] and cost > self.maxcost:
-                self.maxcost = cost
-                self.maxcombo = combo
-        return self.maxcombo, self.maxcost
+        cdef double maxcost = -1
+        cdef double max_capacity = self.csack.get_max_capacity()
+        cdef int combo_num = 0
+        cdef int max_combo_num = 0
+
+        for combo_num in range(2 ** self.csack.get_numitems()):
+            weight, cost = self.csack.compute_weight_and_cost(combo_num)
+            if cost > maxcost and weight <= max_capacity:
+                maxcost = cost
+                max_combo_num = combo_num
+        max_combo = self.int2list(max_combo_num)
+        return max_combo, maxcost
